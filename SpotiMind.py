4049574780736796a9f060bfc6b6f0a8ef3602b7
Brendan Inglis <79180847+bji219@users.noreply.github.com>
2023-05-11 23:12:51 -0400

    added error handling for date diff < 1 day

--- SpotiMind.py
+++ SpotiMind.py
@@ -6,9 +6,9 @@ import smtplib
 from email.message import EmailMessage
 
 # Spotify details
-SPOTIPY_CLIENT_ID=''
-SPOTIPY_CLIENT_SECRET=''
-SPOTIPY_REDIRECT_URI=''
+SPOTIPY_CLIENT_ID='8996b929c79941c9a16d5a53971b4c28'
+SPOTIPY_CLIENT_SECRET='17e4a4c5410f46468c19497b35a62df2'
+SPOTIPY_REDIRECT_URI='https://localhost:8888/callback'
 
 # Authenticate/login to spotify API
 scope = "user-library-read"
@@ -16,12 +16,33 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 # Spotify podcasts to scrape
 spt = {
-    'Your Undivided Attention': '4KI3PtZaWJbAWK89vgttoU' # Spotify show ID
+    'Your Undivided Attention': '4KI3PtZaWJbAWK89vgttoU', # Spotify show ID
+    'The Always Sunny Podcast': '0xDEeqWuoMNBUFGNrhIz6L', # It's always sunny in Philadelphia podcast
+    'On Purpose with Jay Shetty': '5EqqB52m2bsr4k1Ii7sStc',
+    'Approachable Music': '2d0fpifQoLn230QaGjrW7w'
 }
 
 # Google app password
-pw = ''
-email = ''
+pw = 'blfwsvqfbormfbcj'
+email = 'inglisbrendan39@gmail.com'
+
+def no_new_email():
+    # Create a text/plain message
+    msg = EmailMessage()
+    # initialize message
+    body = 'No new podcasts this week. Have a great day!'
+
+    msg.set_content(body)
+
+    msg['Subject'] = 'No New Podcasts This Week!'
+    msg['From'] = email
+    msg['To'] = email
+
+    # context = ssl.create_default_context()
+    # with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email, pw)
+        smtp.sendmail(email, email, msg.as_string())
 
 # Email function
 def send_email(data):
@@ -33,7 +54,7 @@ def send_email(data):
         individual = 'Here is the latest podcast from ' + i[0]['pod_title'] + ':\n\n' + 'Episode #' + str(
             i[0]['ep_num']) + ', \"' + i[0]['show_title'] + '\"\n\n' + i[0][
                          'pod_url'] + '\n\n' + 'Release date: ' + i[0]['r_date'] + '\n\n' + 'Show Notes:\n\n' + i[0][
-                         'description']
+                         'description'] + '\n\n'
 
         body = body + individual
 
@@ -86,12 +107,17 @@ def pod_scrape(val):
     # current date
     todays_date = datetime.datetime.today()
 
-    # Difference between release and today
+    # Difference between today's date and the release date
     date_diff = str(todays_date-latest_release).split(' ')
 
-    # Get num of days
-    day_diff = int(date_diff[0])
+    # Error handling if less than 1 day difference
+    if len(date_diff) == 1:
+        day_diff = 0
+    else:
+        # Get num of days
+        day_diff = int(date_diff[0])
 
+    # If it has been over a week
     if day_diff <= 7:
         return True, data
     else:
@@ -117,4 +143,7 @@ for val in spt.values():
 
 # Send Email if v_array is not empty
 if p_array:
+    print("Sending Email")
     send_email(p_array)
+else:
+    no_new_email()
